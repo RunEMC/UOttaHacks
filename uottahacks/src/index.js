5aa31c5a6f13435f71eb4d6a34504b81196a5782
RunEMC <runlin.li@gmail.com>
2019-02-09 17:48:36 -0500

    Setup base backend

---
+++ uottahacks/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import solace from 'solclientjs';
+
+// Initialize factory with the most recent API defaults
+var factoryProps = new solace.SolclientFactoryProperties();
+factoryProps.profile = solace.SolclientFactoryProfiles.version10;
+solace.SolclientFactory.init(factoryProps);
+
+var session = solace.SolclientFactory.createSession({ 
+    url: "ws://mr4b11zr8yp.messaging.mymaas.net:80", 
+    vpnName: "msgvpn-4b11zr8xv", 
+    userName: "solace-cloud-client", 
+    password: "ndll0aatbdoghi2h6stjmdgg4j", 
+}); 
+try { 
+    session.connect(); 
+    console.log("Connected");
+} catch (error) { 
+    console.log(error); 
+}
+
+ReactDOM.render(<App session={{session}}/>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+
+
+if (module.hot) module.hot.accept();
