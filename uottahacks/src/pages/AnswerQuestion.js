60cb1d299f3aa429a3c18b19446b2832c6708790
RunEMC <runlin.li@gmail.com>
2019-02-10 05:32:44 -0500

    Almost finish web

--- uottahacks/src/pages/AnswerQuestion.js
+++ uottahacks/src/pages/AnswerQuestion.js
@@ -3,14 +3,16 @@ import { CssBaseline, withStyles, Button, TextField, Card, CardContent, Expansio
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CardActions from '@material-ui/core/CardActions';
 
-import TopicPublisher from '../services/TopicSubscriber';
+import TopicPublisher from '../services/TopicPublisher';
 import TopicSubscriber from '../services/TopicSubscriber';
 
 const styles = theme => ({
   mainContainer: {
-    backgroundColor: '#85C7F2',
-    minHeight: '100vh',
-    height: '100%'
+    margin: '15px',
+    padding: '15px'
+  },
+  container: {
+    margin: '35px'
   },
   main: {
     zIndex: -1,
@@ -19,6 +21,9 @@ const styles = theme => ({
     height: '100%',
     padding: '2% 10% 6% 10%',
     boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)'
+  },
+  button: {
+    marginRight: '15px'
   }
 });
 
@@ -28,7 +33,9 @@ class AnswerQuestion extends React.Component {
 
     this.state = {
       session: props.session,
-      input: ''
+      input: '',
+      questions: [],
+      curPos: 0
     }
 
     this.sendMsg = this.sendMsg.bind(this);
@@ -48,13 +55,23 @@ class AnswerQuestion extends React.Component {
   }
 
   sendMsg() {
-    console.log("Sending msg" + this.state.session);
+    this.setState({
+      curPos: this.state.curPos + 1
+    });
+    if (this.state.curPos >= this.state.questions.length) {
+
+    }
     // create the publisher, specifying the name of the subscription topic
     // var publisher = new TopicPublisher(this.state.session, 'tutorial/topic');
     // publisher.publish(this.state.input);
   }
 
   updateAndSetQuestions(question) {
+    var questions = this.state.questions;
+    questions.push(question);
+    this.setState({
+      questions: questions
+    });
     console.log(question)
   }
 
@@ -67,24 +84,31 @@ class AnswerQuestion extends React.Component {
 
     return(
     <div className={classes.mainContainer}>
-     <Card className={classes.card} styles="width:10px;height:10px;">
-    <CardContent>
-       <Typography gutterBottom variant="h5" component="h2">
-       Question 1</Typography>
-    </CardContent>
-    <CardActions>
-        <Button size="small">Answer This Question</Button>
-      </CardActions>
-    </Card>
-    
-    <div>
-       <form>
-       <textarea styles="font-family:Helvetica;font-size:12px;" align="center"
-       value="Enter your answer here..." rows="15" cols="50"/>
-       <br/>
-          <input type="submit" value="Submit Answer" align="right"/>
-       </form>
-    </div>
+      <Card className={classes.card} styles="width:10px;height:10px;">
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {this.state.questions[this.state.curPos]}
+          </Typography>
+        </CardContent>
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+          id="response"
+          label="Response"
+          multiline
+          className={classes.textField}
+          value={this.state.input}
+          onChange={this.handleChange('input')}
+          margin="normal"
+          fullWidth
+          />
+          <Button variant="contained" disabled={this.state.isDone} className={classes.button} onClick={this.sendMsg}>
+            Next
+          </Button>
+          <Button variant="contained" disabled={this.state.isDone} className={classes.button} onClick={this.sendMsg}>
+            Skip
+          </Button>
+        </form>
+      </Card>
     
     </div>
     );
