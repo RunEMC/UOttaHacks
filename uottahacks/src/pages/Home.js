b8d60dfe463a523ab765db5e835b1ca96d969ead
RunEMC <runlin.li@gmail.com>
2019-02-10 03:09:43 -0500

    Add synch across clients

--- uottahacks/src/pages/Home.js
+++ uottahacks/src/pages/Home.js
@@ -1,30 +1,35 @@
 import React, { Fragment } from 'react';
 import { CssBaseline, withStyles, Button, TextField, Paper, List, ListItem, Typography } from '@material-ui/core';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import CheckmarkIcon from '@material-ui/icons/CheckCircleOutline'
 
 import TopicPublisher from '../services/TopicPublisher';
 import TopicSubscriber from '../services/TopicSubscriber';
 
 const styles = theme => ({
-  headingOne:{
-fontSize: 56,
-  },
-  headingTwo:{
-    fontSize: 32,
-      },
-  mainContainer: {
-    backgroundColor: '#85C7F2',
-    minHeight: '100vh',
-    height: '100%'
-  },
-  main: {
-    zIndex: -1,
-    backgroundColor: '#F1F1F1',
-    margin: '40px 12% 40px 12%',
-    height: '100%',
-    padding: '2% 10% 6% 10%',
-    boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)'
-  }
+    mainContainer: {
+        padding: '15px',
+        margin: '25px'
+    },
+    mainContainer: {
+        padding: '15px',
+        margin: '25px'
+    },
+    container: {
+        padding: '15px',
+        margin: '25px'
+    },
+    main: {
+        zIndex: -1,
+        backgroundColor: '#F1F1F1',
+        margin: '40px 12% 40px 12%',
+        height: '100%',
+        padding: '2% 10% 6% 10%',
+        boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)'
+    },
+    userField: {
+        marginLeft: '15px'
+    }
 });
 
 class Home extends React.Component {
@@ -36,13 +41,21 @@ class Home extends React.Component {
       input: '',
       isSignedIn: false,
       isBlank: true,
-      users: []
+      users: [],
+      isDone: false,
+      readyStatus: "Ready Up",
+      userStatus: {},
+      user: ''
     }
 
     this.sendMsg = this.sendMsg.bind(this);
+    this.registerDone = this.registerDone.bind(this);
     
     var idSubscriber = new TopicSubscriber(this, 'userid');
     idSubscriber.run();
+
+    var statusSubscriber = new TopicSubscriber(this, 'userstatus');
+    statusSubscriber.run();
   }
 
   sendMsg() {
@@ -51,15 +64,38 @@ class Home extends React.Component {
     var publisher = new TopicPublisher('userid');
     publisher.publish(this.state.input);
     this.setState({
-        isSignedIn: true
+        isSignedIn: true,
+        user: this.state.input
+    });
+  }
+
+  registerDone() {
+    var publisher = new TopicPublisher('userstatus');
+    publisher.publish(this.state.user);
+    var ready = this.state.isDone ? "Ready Up" : "Unready";
+    this.setState({
+        isSignedIn: true,
+        readyStatus: ready,
+        isDone: !this.state.isDone
+    });
+  }
+
+  updateUserStatus(user) {
+    var statuses = this.state.userStatus;
+    statuses[user] = !this.state.userStatus[user];
+    this.setState({
+        userStatus: statuses
     });
   }
 
   updateUsers(name) {
     var us = this.state.users;
     us.push(name);
+    var statuses = this.state.userStatus;
+    statuses[name] = false;
     this.setState({
-      users: us
+      users: us,
+      userStatus: statuses
     });
   }
 
@@ -78,14 +114,17 @@ class Home extends React.Component {
 
   render() {
     const { classes } = this.props;
-    const listNames = this.state.users.map((user) => (
-      <ListItem><Typography>{user}</Typography></ListItem>
-    ));
+    const listNames = this.state.users.map((user) => {
+        const status = this.state.userStatus[user];
+        return (
+            <ListItem><CheckmarkIcon style={{visibility: status ? 'visible' : 'hidden' }}/><Typography className={classes.userField}>{user}</Typography></ListItem>
+        );
+    });
 
     return (
-        <Paper>
-            <h1 className={classes.headingOne}>Welcome</h1>
-            <h2 className={classes.headingTwo}>I want to:</h2>
+        <Paper className={classes.mainContainer}>
+            <Typography variant="h4">YesBrainer</Typography>
+            <Typography variant="body1">Enter your name below to join a study group, press done when everyone in your group is ready to begin the session!</Typography>
             <form className={classes.container} noValidate autoComplete="off">
                 <TextField
                 disabled={this.state.isSignedIn}
@@ -98,18 +137,18 @@ class Home extends React.Component {
                 fullWidth
                 />
                 <Button disabled={this.state.isSignedIn || this.state.isBlank} className={classes.button} onClick={this.sendMsg}>
-                    Sign In
+                    Register
                 </Button>
             </form>
             <div style={{visibility: this.state.users.length ? 'visible' : 'hidden' }}>
-                <Paper>
+                <Paper className={classes.container}>
                     <List className={classes.root}>
                     <Typography className={classes.grid}>Current Students</Typography>
                         {listNames}
                     </List>
                 </Paper>
                 <Button className={classes.button} onClick={this.registerDone}>
-                    Done
+                    {this.state.readyStatus}
                 </Button>
             </div>
         </Paper>
